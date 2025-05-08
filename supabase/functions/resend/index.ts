80cfa90d53cd553566b47ec7e0be4c1e054de5ae
Uwe Schwarz <4044886+uwe-schwarz@users.noreply.github.com>
2025-05-08 13:24:08 +0200

    fix(deps): Aktualisiere Deno Standard Library auf Version 0.224.0

--- supabase/functions/resend/index.ts
+++ supabase/functions/resend/index.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
 import { corsHeaders } from '../_shared/cors.ts'
 
 const handler = async (request: Request): Promise<Response> => {
